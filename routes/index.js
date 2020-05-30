b9e5e1be49ed0f976e0da0bbb3d7b3dd2fca75f9
hiroin <egamescape@gmail.com>
2020-05-30 23:55:22 +0900

    test.pugの追加

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 const Ooparts = require('../models/ooparts');
 
+router.get('/test', (req, res, next) => {
+  res.render('test', { title: 'OOpartsゲーム一覧' });
+});
+
 router.get('/', (req, res, next) => {
   Ooparts.fetchOOpartsGames().then((Games) => {
     res.render('index', { title: 'OOpartsゲーム一覧', games: Games, path: '/' });
